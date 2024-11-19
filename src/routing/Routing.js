ee74894bb15686bece6279e121a40d471e3d8615
Usman Khan <usman918khan@gmail.com>
2024-11-19 22:21:57 +0500

    working-admin-routing

--- src/routing/Routing.js
+++ src/routing/Routing.js
@@ -1,45 +1,45 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import PrivateRoute from "./PrivateRoute";
-import Home from "../pages/home/Home";
-import Signup from "../pages/signup/SignUp";
-import Login from "../pages/login/Login";
-import PublicRoute from "./PublicRoute";
-import ContactUs from "../pages/contectus/ContactUs";
-import MenuPage from "../pages/menu/MenuPage";
-import About from "../pages/about/About";
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+// import PrivateRoute from "./PrivateRoute";
+// import Home from "../pages/home/Home";
+// import Signup from "../pages/signup/SignUp";
+// import Login from "../pages/login/Login";
+// import PublicRoute from "./PublicRoute";
+// import ContactUs from "../pages/contectus/ContactUs";
+// import MenuPage from "../pages/menu/MenuPage";
+// import About from "../pages/about/About";
 
-const router = createBrowserRouter([
-  {
-    path: "/home",
-    element: <PrivateRoute><Home /></PrivateRoute>,
-  },
-  {
-    path: "/",
-    element: <PublicRoute><Signup /></PublicRoute>
-  },
-  {
-    path: "/login",
-    element: <PublicRoute><Login /></PublicRoute>
-  },
-  {
-    path: "/ContectUs",
-    element: <PrivateRoute><ContactUs /></PrivateRoute>
-  },
-  {
-    path: "/menupage",
-    element: <PrivateRoute><MenuPage /></PrivateRoute>
-  },
-  {
-    path: "/about",
-    element: <PrivateRoute><About /></PrivateRoute>
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/home",
+//     element: <PrivateRoute><Home /></PrivateRoute>,
+//   },
+//   {
+//     path: "/",
+//     element: <PublicRoute><Signup /></PublicRoute>
+//   },
+//   {
+//     path: "/login",
+//     element: <PublicRoute><Login /></PublicRoute>
+//   },
+//   {
+//     path: "/ContectUs",
+//     element: <PrivateRoute><ContactUs /></PrivateRoute>
+//   },
+//   {
+//     path: "/menupage",
+//     element: <PrivateRoute><MenuPage /></PrivateRoute>
+//   },
+//   {
+//     path: "/about",
+//     element: <PrivateRoute><About /></PrivateRoute>
+//   },
+// ]);
 
-export default function Routing(params) {
-  return (
-    <RouterProvider router={router} />
-  )
-}
+// export default function Routing(params) {
+//   return (
+//     <RouterProvider router={router} />
+//   )
+// }
